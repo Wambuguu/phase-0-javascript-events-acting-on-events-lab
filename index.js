997a80ce97de61833e79c7f347ca346921cfeaeb
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-14 17:24:58 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left) || 0;
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    const left = parseInt(dodger.style.left) || 0;
+    const rightGameBoundary = window.innerWidth - dodger.offsetWidth;
+    if (left < rightGameBoundary) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
